3b97bc47a153d37e3131f3fc622539d18726775e
Keagan McClelland <kmcclel2@illinois.edu>
2014-12-10 13:21:28 -0600

    dependencies

--- hello.go
+++ hello.go
@@ -1,7 +1,19 @@
 package main
 
 import "fmt"
+import "net/http"
+import "os"
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", hello)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "hello, world")
+	fmt.Println("response sent")
 }
